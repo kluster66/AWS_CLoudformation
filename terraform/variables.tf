7aa8cd928cc981db30ec8a3146590e4513ab7109
kluster66 <pbazerbe@gmail.com>
2018-02-13 18:23:52 +0100

    modifié :         terraform/provider.tf 	modifié :         terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,10 @@ default = "eu-west-3"
 
 variable "aws_ami" {
   default = {
-        eu-west-3 = "ami-d834aba1
-        }"
+        eu-west-3 = "ami-8ee056f3"
+  }
+variable "aws_key_name" {
+  default ="ParisKP"
+  }
 
 }
