cd90556c62ef8916886f21c78eb308a3cfcd1f4a
kluster66 <pbazerbe@gmail.com>
2018-02-14 14:40:48 +0100

    a

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -4,10 +4,14 @@ resource "aws_instance" "server01"{
   ami ="${lookup(var.aws_ami, var.aws_region)}"
   key_name = "${var.aws_key_name}"
   security_groups = "${var.aws_security_group}"
+
   tags {
     Name = "Terraform_example"
   }
 #mise a jour de la vm et installation des packages
-
-
+  user_data = <<-EOF
+              #!bin/bash
+              sudo yum update
+              sudo yum install http
+              EOF
 }
