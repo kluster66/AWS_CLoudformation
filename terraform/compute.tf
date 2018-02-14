1649384c6c3f84c0e0a8d653670be361cd672308
kluster66 <pbazerbe@gmail.com>
2018-02-14 15:53:10 +0100

    a

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -3,12 +3,13 @@ resource "aws_instance" "server01"{
   instance_type ="t2.micro"
   ami ="${lookup(var.aws_ami, var.aws_region)}"
   key_name = "${var.aws_key_name}"
-  security_groups = "${var.aws_security_group}"
+
 
   subnet_id = "${aws_subnet.public_subnet.id}"
   tags {
     Name = "Terraform_example"
   }
+  security_groups = "${var.aws_security_group}"
 #mise a jour de la vm et installation des packages
   user_data = <<-EOF
               #!bin/bash
