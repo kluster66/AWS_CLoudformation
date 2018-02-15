886311ebc0183570285bc671655f6faae703ae1b
kluster66 <pbazerbe@gmail.com>
2018-02-15 10:46:32 +0100

    a

--- terraform/dirty_test/network.tf
+++ terraform/dirty_test/network.tf
@@ -51,3 +51,14 @@ resource "aws_internet_gateway" "gw" {
     Name = "MyInternetGW"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+      Name = "Public"
+  }
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
