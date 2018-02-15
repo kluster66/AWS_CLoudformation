18099916288cf865ba823f6762e81a0f7535048d
kluster66 <pbazerbe@gmail.com>
2018-02-15 15:45:37 +0100

    a

--- terraform/dirty_test/network-base.tf
+++ terraform/dirty_test/network-base.tf
@@ -43,26 +43,3 @@ resource "aws_subnet" "public_subnet" {
     Name = "MyPublicSubnet"
   }
 }
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  tags {
-    Name = "MyInternetGW"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.main.id}"
-  tags {
-      Name = "Public"
-  }
-  route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.gw.id}"
-    }
-}
-resource "aws_main_route_table_association" "a" {
-  vpc_id         = "${aws_vpc.main.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
