5968a08fdb73acd82ace2467b0ac08178fcfa516
kluster66 <pbazerbe@gmail.com>
2018-04-11 17:03:51 +0200

    faut de frappe

--- terraform/user_mgt/variables.tf
+++ terraform/user_mgt/variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
-default = "eu-west-3"
+  default = "eu-west-3"
 }
 
 variable "key_path" {
-    default = "/home/philippe/dev/aws/ssh_keys/zorglub/zorglub.pem"
+  default = "/home/philippe/dev/aws/ssh_keys/zorglub/zorglub.pem"
 
 }
 
 Variable "user_name_prefix" {
-    Default = "Soget"
+  default = "Soget"
 
 }
