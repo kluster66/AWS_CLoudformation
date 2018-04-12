4cb3cd078692cb3098b5e9ae1a2594c7e116a3ee
kluster66 <pbazerbe@gmail.com>
2018-04-12 14:05:35 +0200

    &

--- terraform/user_mgt/user.tf
+++ terraform/user_mgt/user.tf
@@ -2,17 +2,3 @@ resource "aws_iam_user" "students" {
   count = 1
   name = "${var.user_name_prefix}0${count.index}"
 }
-
-
-resource "aws_iam_group" "students_group" {
-  name = "students"
-}
-
-resource "aws_iam_user_login_profile" "students" {
-  user = "${aws_iam_user.students.name}"
-  pgp_key = "keybase:philippe"
-}
-
-output "password" {
-  value = "${aws_iam_user_login_profile.students.encrypted_password}"
-}
