c7741d0b8628125efb8306289a83e39ceaf4ddb5
kluster66 <pbazerbe@gmail.com>
2018-04-12 09:59:14 +0200

    aa

--- terraform/user_mgt/user.tf
+++ terraform/user_mgt/user.tf
@@ -6,7 +6,8 @@ resource "aws_iam_user" "students" {
 
 resource "aws_iam_user_policy" "st_policy" {
   user = "${aws_iam_user.students.name}"
-  policy = <<EOF {
+  policy = <<EOF
+  {
     "Version": "2012-10-17",
     "Statement": [
         {
@@ -58,6 +59,6 @@ resource "aws_iam_user_policy" "st_policy" {
         }
     ]
   }
-  EOF
+EOF
 
 }
