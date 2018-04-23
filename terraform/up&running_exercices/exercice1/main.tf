3b951801996207201b30e7593d145f52af149c3a
kluster66 <pbazerbe@gmail.com>
2018-04-23 16:52:02 +0200

    ssh

--- terraform/up&running_exercices/exercice1/main.tf
+++ terraform/up&running_exercices/exercice1/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-8ee056f3"
   instance_type = "t2.micro"
+  key_name = "${var.aws_key_name}"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
@@ -18,6 +19,12 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port = 8080
     to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- terraform/up&running_exercices/exercice1/variables.tf
+++ terraform/up&running_exercices/exercice1/variables.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
   default = "eu-west-3"
 }
 
+variable "aws_key_name" {
+  default ="zorglub"
+}
+
 variable "key_path" {
   default = "/home/philippe/dev/aws/ssh_keys/zorglub/zorglub.pem"
 
