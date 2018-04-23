30b6a68f571a1b3e688914f40639e634b6a1255a
kluster66 <pbazerbe@gmail.com>
2018-04-23 15:54:16 +0200

    first commit

---
+++ terraform/up&running_exercices/exercice1/main.tf
@@ -0,0 +1,23 @@
+ressource "aws_instance" "example" {
+  ami           = "ami-8ee056f3"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "hello world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform/up&running_exercices/exercice1/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "eu-west-3"
+}
+
+variable "key_path" {
+  default = "/home/philippe/dev/aws/ssh_keys/zorglub/zorglub.pem"
+
+}
