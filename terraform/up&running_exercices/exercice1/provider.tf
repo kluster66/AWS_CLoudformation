26d7d420760b08d939aaa4dff2a2212fcf8d9f66
kluster66 <pbazerbe@gmail.com>
2018-04-24 14:18:42 +0200

    on pousse

--- terraform/up&running_exercices/exercice1/provider.tf
+++ terraform/up&running_exercices/exercice1/provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region     = "${var.aws_region}"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-tls"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
