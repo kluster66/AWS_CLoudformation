1b059fb612528cd4f971eca1dc900fa3aa3d1e05
kluster66 <pbazerbe@gmail.com>
2018-04-25 14:54:11 +0200

    policy test

--- terraform/user_mgt/user.tf
+++ terraform/user_mgt/user.tf
@@ -2,3 +2,9 @@ resource "aws_iam_user" "students" {
   count = 1
   name = "${var.user_name_prefix}0${count.index}"
 }
+
+resource "aws_iam_policy" "policy" {
+  name        = "students-policy"
+  description = "A test policy"
+  policy      = # omitted
+}
