fa630a7c4b9363eac1354f2689d3d1488e59cf75
kluster66 <pbazerbe@gmail.com>
2018-04-25 16:45:54 +0200

    a

--- terraform/user_mgt/output.tf
+++ terraform/user_mgt/output.tf
@@ -2,5 +2,5 @@ output "users_name" {
   value = "${aws_iam_user.students.*.name}"
 }
 output "password" {
-  value = "${aws_iam_user_login_profile.students.encrypted_password}"  
+  value = "${aws_iam_user_login_profile.students.*.encrypted_password}"
 }
