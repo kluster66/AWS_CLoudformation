c603c8f0c571f7cc79c216ec63045e0407ed7042
kluster66 <pbazerbe@gmail.com>
2018-04-25 14:57:46 +0200

    policy suite du test

--- terraform/user_mgt/user.tf
+++ terraform/user_mgt/user.tf
@@ -6,5 +6,58 @@ resource "aws_iam_user" "students" {
 resource "aws_iam_policy" "policy" {
   name        = "students-policy"
   description = "A test policy"
-  policy      = # omitted
+  policy      = <<-EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "VisualEditor0",
+              "Effect": "Allow",
+              "Action": "ec2:RunInstances",
+              "Resource": "arn:aws:ec2:eu-west-3:426877532519:instance/*",
+              "Condition": {
+                  "StringEquals": {
+                      "ec2:InstanceType": "t2.micro"
+                  }
+              }
+          },
+          {
+              "Sid": "VisualEditor1",
+              "Effect": "Allow",
+              "Action": "ec2:RunInstances",
+              "Resource": "arn:aws:ec2:eu-west-3::image/ami-*",
+              "Condition": {
+                  "StringEquals": {
+                      "ec2:Owner": "amazon"
+                  }
+              }
+          },
+          {
+              "Sid": "VisualEditor2",
+              "Effect": "Allow",
+              "Action": [
+                  "ec2:DescribeImages",
+                  "ec2:DescribeInstances",
+                  "ec2:DescribeVpcs",
+                  "ec2:DescribeSubnets",
+                  "ec2:DescribeKeyPairs",
+                  "ec2:DescribeSecurityGroups"
+              ],
+              "Resource": "*"
+          },
+          {
+              "Sid": "VisualEditor3",
+              "Effect": "Allow",
+              "Action": "ec2:RunInstances",
+              "Resource": [
+                  "arn:aws:ec2:eu-west-3:426877532519:volume/*",
+                  "arn:aws:ec2:eu-west-3:426877532519:subnet/*",
+                  "arn:aws:ec2:eu-west-3:426877532519:key-pair/*",
+                  "arn:aws:ec2:eu-west-3:426877532519:network-interface/*",
+                  "arn:aws:ec2:eu-west-3:426877532519:security-group/*"
+              ]
+          }
+      ]
+  }
+  EOF
 }
